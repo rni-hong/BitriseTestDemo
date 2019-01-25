2ac3e1b0607696869edf32844fc2c1ef2260cd93
rni-hong <c.hong@r-n-i.jp>
2019-01-25 12:38:13 +0900

    Initial Commit

---
+++ BitriseTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BitriseTestDemo
+//
+//  Created by hong on 2019/01/25.
+//  Copyright © 2019 r-n-i. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BitriseTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BitriseTestDemo
+//
+//  Created by hong on 2019/01/25.
+//  Copyright © 2019 r-n-i. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
